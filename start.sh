2b1630804342cbc7ac012338c2b03559ab0bea05
Jayy <86412100+jayythecracker@users.noreply.github.com>
2025-03-17 03:15:41 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 npm ci
 
 # Build the application
-npm run build
+npx vite build && npx esbuild server/index.ts --platform=node --packages=external --bundle --format=esm --outdir=dist
 
 # Start the production server
 NODE_ENV=production node dist/index.js
